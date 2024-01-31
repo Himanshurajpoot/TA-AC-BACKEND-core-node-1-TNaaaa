44b957829aa72392f1aa162d76295448ac6f8e69
Himanshu <harryrajpoot2003@gmail.com>
2024-01-31 17:40:53 +0530

    completed

---
+++ block-BNaaat/index.js
@@ -0,0 +1,24 @@
+let http = require("http")
+let fs = require("fs")
+
+let server = http.createServer(handleRequest)
+
+
+ function handleRequest(req, res){
+      if(req.method==="GET" && req.url==="/file"){
+        
+        fs.readFile("./node.html" , (err, content)=>{
+           if(err) console.log(err);
+           res.setHeader('Content-Type', 'text/html')
+           res.end(content)
+        })
+      }
+       if(req.method==="GET" && req.url==="/stream"){
+        res.setHeader('Content-Type', 'text/html')
+        fs.createReadStream("./node.html").pipe(res)
+      }
+ }
+
+ server.listen(5555, ()=>{
+    console.log(" server is listening on port 5555 ")
+ })
